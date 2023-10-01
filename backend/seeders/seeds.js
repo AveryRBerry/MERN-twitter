b2c30e078bc732bbe5d90031b5269bcf6d1c46fd
Avery Berry <AveryRBerry@gmail.com>
2023-10-01 16:44:21 -0400

    fixed seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
 const Tweet = require('../models/Tweet');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
-// require('dotenv').config();
+// console.log(db)
+
 
 const NUM_SEED_USERS = 10;
 const NUM_SEED_TWEETS = 30;
