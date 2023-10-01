06d646be6e83b66de4f713e61d02ce7772f067c8
Avery Berry <AveryRBerry@gmail.com>
2023-09-30 20:59:17 -0400

    finnished valadations

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,8 +1,14 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI
-}
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI
+// }
+
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   isProduction: process.env.NODE_ENV === 'production'
+// }
 
 module.exports = {
+  secretOrKey: process.env.SECRET_OR_KEY,
   mongoURI: process.env.MONGO_URI,
   isProduction: process.env.NODE_ENV === 'production'
-}
+};
